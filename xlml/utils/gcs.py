0c2917bd0e703f09ad3a8f518ba8de1d46ad9f38
Ernie Chang <changernie@google.com>
2025-06-05 22:29:59 +0800

    update

--- xlml/utils/gcs.py
+++ xlml/utils/gcs.py
@@ -13,9 +13,11 @@ def validate_gcs_checkpoint_p2(output_path):
   logging.info(f"bucket:{bucket_name}")
   logging.info(f"prefix:{prefix}")
   files = hook.list(bucket_name=bucket_name, prefix=prefix)
+  print(files, type(files))
   logging.info("Files ===> ", files)
   if len(files) > 0:
     for file in files:
       if ".data" in file:
+        print(file)
         return True
   return False
