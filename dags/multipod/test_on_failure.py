c706cb45299147ca023101886559d5e01d5e08d7
severusho <severus.ho@cienet.com>
2025-06-19 17:54:57 +0800

    update

--- dags/multipod/test_on_failure.py
+++ dags/multipod/test_on_failure.py
@@ -5,11 +5,7 @@ from datetime import datetime, timedelta
 
 from airflow.decorators import task
 from airflow.exceptions import AirflowException
-from airflow.utils.task_group import TaskGroup
-from airflow.utils.trigger_rule import TriggerRule
-from airflow.operators.python import get_current_context
-from airflow.providers.github.operators.github import GithubOperator
-from github import Repository
+
 
 """
 1. add 'apache-airflow-providers-github' at composer/Pypi packages/Edit
@@ -35,42 +31,12 @@ def task_c():
   logging.info("task C")
 
 
-def create_issue(repo: Repository):
-  context = get_current_context()
-  dag_run = context['dag_run']
-  failed_tis = dag_run.get_task_instances(state='failed')
-  error_msg = ""
-  for ti in failed_tis:
-      # Check Issue body format
-      logging.error(f"DAG: {ti.dag_id}, Run: {ti.run_id}, Task: {ti.task_id} failed")
-      error_msg += (f"DAG: {ti.dag_id}, Run: {ti.run_id}, Task: {ti.task_id} "
-                    f"failed\n")
-  repo.create_issue(
-      title=f'error_dag',
-      body=f'{error_msg}',
-  )
-
-
-def on_failure():
-  return GithubOperator(
-      task_id="on_failure",
-      github_conn_id="github_default",
-      github_method="get_repo",
-      github_method_args={
-          # Make Variables
-          "full_name_or_id": "CIeNET-International/ml-auto-solutions-2",
-      },
-      result_processor=create_issue,
-      trigger_rule=TriggerRule.ONE_FAILED,
-      do_xcom_push=False, # Make try not to serialize Issue into XCom
-  )
-
-
 with models.DAG(
     dag_id="test_on_failure",
     schedule=None,
     tags=[
       "test_dag",
+      "github_enabled",
     ],
     catchup=False,
     default_args={
@@ -78,10 +44,8 @@ with models.DAG(
         'retry_delay': timedelta(minutes=5),
     }
 ) as dag:
-  with TaskGroup('group1') as gp1:
-    taskA = task_a()
-  with TaskGroup('group2') as gp2:
-    taskB = task_b()
+  taskA = task_a()
+  taskB = task_b()
   taskC = task_c()
-  onFailure = on_failure()
-  gp1 >> gp2 >> taskC >> onFailure
+  taskA >> taskB >> taskC
+
